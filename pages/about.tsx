b87fda2548c89a0c8efac058e3f44c78da063b36
YehorBelenkov <yehorbelenkov@gmail.com>
2023-08-01 16:01:08 -0500

    chabges

---
+++ pages/about.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutPage: React.FC = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Welcome to our About page! We are a magical shop selling fairy products.</p>
+    </div>
+  );
+};
+
+export default AboutPage;
